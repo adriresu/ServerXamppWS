b0d4086c4e8350cd86762ff7b6cfbd24217bae0f
Adrian Resua Vidal <resuavidal@gmail.com>
2022-11-26 19:31:32 +0100

    Updated with server requests (2)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     if (isset($_POST['username']) && isset($_POST['password']))
     {
       //Mostrar un post
-      $sql = $dbConn->prepare("SELECT Usuario, Contrasenha FROM usuario WHERE Usuario=:usuario AND Contrasenha=:contrasenha");
+      $sql = $dbConn->prepare("SELECT * FROM usuario WHERE Usuario=:usuario AND Contrasenha=:contrasenha");
       $sql->bindValue(':usuario', $_POST['username']);
       $sql->bindValue(':contrasenha', $_POST['password']);
       $sql->execute();
@@ -18,19 +18,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
       $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
 
       if (count($dataReceived) > 0) {
-        $response = array('response' => 'True');
+        echo json_encode($dataReceived);
       }
-      else{
-        $response = array('response' => 'False');
-      }
-      echo json_encode($response);
       exit();
 	  }
     else{
-      $response->response = 'False';
-      echo json_encode($response);
     }
-
   }
   else if($_POST['Tipo'] == 'Register'){
     if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
@@ -46,22 +39,56 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
       $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
 
       if (count($dataReceived) > 0) {
-        $response = array('response' => 'True');
+        echo json_encode($dataReceived);
       }
-      else{
-        $response = array('response' => 'True');
-      }
-      echo json_encode($response);
       exit();
     }
     else{
       $response->response = 'False';
       echo json_encode($response);
     }
-
   }
+
+  else if($_POST['Tipo'] == 'CheckIfUser'){
+    if (isset($_POST['username']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * FROM usuario WHERE Usuario=:usuario");
+      $sql->bindValue(':usuario', $_POST['username']);
+      $sql->execute();
+
+      header("HTTP/1.1 200 OK");
+      $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+      if (count($dataReceived) > 0) {
+        echo json_encode($dataReceived);
+      }
+      exit();
+	  }
+    else{
+    }
+  }
+  
   else if($_POST['Tipo'] == 'Series'){
-    $sql = $dbConn->prepare("SELECT * FROM serie");
+    $sql = $dbConn->prepare("SELECT * FROM serie WHERE Tipo=0");
+    $sql->execute();
+    header("HTTP/1.1 200 OK");
+    $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+    if (count($dataReceived) > 0) {
+      echo json_encode($dataReceived);
+    }
+  }
+  else if($_POST['Tipo'] == 'Peliculas'){
+    $sql = $dbConn->prepare("SELECT * FROM serie WHERE Tipo=1");
+    $sql->execute();
+    header("HTTP/1.1 200 OK");
+    $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+    if (count($dataReceived) > 0) {
+      echo json_encode($dataReceived);
+    }
+  }
+  else if($_POST['Tipo'] == 'Animes'){
+    $sql = $dbConn->prepare("SELECT * FROM serie WHERE Tipo=2");
     $sql->execute();
     header("HTTP/1.1 200 OK");
     $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
@@ -99,7 +126,78 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
       echo json_encode($dataReceived);
     }
   }
-  
+  elseif ($_POST['Tipo'] == 'SerieName') {
+    $sql = $dbConn->prepare("SELECT * FROM serie");
+    $sql->execute();
+    header("HTTP/1.1 200 OK");
+    $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+    if (count($dataReceived) > 0) {
+      echo json_encode($dataReceived);
+    }
+  }
+  elseif ($_POST['Tipo'] == 'SerieInfo') {
+    $sql = $dbConn->prepare("SELECT * FROM usuario WHERE Usuario=:usuario");
+    $sql->bindValue(':usuario', $_POST['usuario']);
+    $sql->execute();
+    header("HTTP/1.1 200 OK");
+    $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+    if (count($dataReceived) > 0) {
+      echo json_encode($dataReceived);
+    }
+  }
+
+  elseif ($_POST['Tipo'] == 'SerieExtraInfo') {
+    $sql = $dbConn->prepare("SELECT * FROM usuarios_serie WHERE ID_Usuario=:usuario AND ID_Serie=:serie");
+    $sql->bindValue(':usuario', $_POST['idUser']);
+    $sql->bindValue(':serie', $_POST['id']);
+    $sql->execute();
+    header("HTTP/1.1 200 OK");
+    $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+    if (count($dataReceived) > 0) {
+      echo json_encode($dataReceived);
+    }
+  }
+
+  else if($_POST['Tipo'] == 'serieViewed'){
+    if (isset($_POST['id']) && isset($_POST['idUser'])) {
+      $viewed = intval($_POST['viewed']);
+
+      $sql = $dbConn->prepare("SELECT * FROM usuarios_serie WHERE ID_Usuario=:usuario AND ID_Serie=:serie");
+      $sql->bindValue(':usuario', $_POST['idUser']);
+      $sql->bindValue(':serie', $_POST['id']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      $dataReceivedSelect = $sql->fetchAll(PDO::FETCH_ASSOC);
+      
+      if (count($dataReceivedSelect) > 0){
+        $sql = $dbConn->prepare("UPDATE usuarios_serie SET Estado = :viewed WHERE ID_Usuario=:usuario AND ID_Serie=:serie");
+        $sql->bindValue(':usuario', $_POST['idUser']);
+        $sql->bindValue(':serie', $_POST['id']);
+        $sql->bindValue(':viewed', $viewed);
+        $sql->execute();
+        header("HTTP/1.1 200 OK");
+        $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+        $response = array('response' => 'True');
+      }
+      else{
+        $sql = $dbConn->prepare("INSERT INTO usuarios_serie(ID_usuario, ID_serie, Estado) VALUES (:usuario,:serie,:viewed)");
+        $sql->bindValue(':usuario', $_POST['idUser']);
+        $sql->bindValue(':serie', $_POST['id']);
+        $sql->bindValue(':viewed', $viewed);
+        $sql->execute();
+        header("HTTP/1.1 200 OK");
+        $dataReceived = $sql->fetchAll(PDO::FETCH_ASSOC);
+        $response = array('response' => 'True');
+      }
+
+      echo json_encode($response);
+      exit();
+    }
+    else{
+      $response->response = 'False';
+      echo json_encode($response);
+    }
+  }
 
   else {
     //Mostrar lista de post
